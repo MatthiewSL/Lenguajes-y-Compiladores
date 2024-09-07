fae28b400f15a7150761a423e551f51082121a5b
Martin Didolich <marindidolich2012@gmail.com>
2024-09-07 15:09:41 -0300

    Entrega

--- Primera entrega/run.bat
+++ Primera entrega/run.bat
@@ -6,11 +6,4 @@ gcc.exe lex.yy.c y.tab.c -o lyc-compiler-1.0.0.exe
 
 lyc-compiler-1.0.0.exe prueba.txt
 
-@echo off
-del lyc-compiler-1.0.0.exe
-del lex.yy.c
-del y.tab.c
-del y.tab.h
-del y.output
-
 pause
